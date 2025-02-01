e3a446cc397c491d7470436f15e768646dea1181
George S. Mulbah <gsmulbah2500@gmail.com>
2025-02-01 03:33:36 +0200

    added the backend schema

---
+++ apps/iduka-mart_backend/src/main/java/com/bits/idukamart/IdukaMartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bits.idukamart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdukaMartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdukaMartBackendApplication.class, args);
+	}
+
+}
---
+++ apps/iduka-mart_backend/src/test/java/com/bits/idukamart/IdukaMartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bits.idukamart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdukaMartBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
